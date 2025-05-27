e4c2e0e77e85ed423ef5f4ffaa83f428191af337
NguyenLeMinhTu <nlminhtu.2404@gmail.com>
2025-05-27 19:15:23 +0700

    backend: create model

---
+++ BackEnd/model/productsModel.js
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const productSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    description: { type: String, required: true },
+    price: { type: mongoose.Schema.Types.Mixed, required: true },
+    images: { type: Array, required: true },
+    category: { type: String, required: true },
+    subCategory: { type: String, required: true },
+    sizes: { type: Array, required: true },
+    bestseller: { type: Boolean, required: true },
+    date: { type: Number, required: true },
+});
+
+const ProductModel = mongoose.models.product || mongoose.model('product', productSchema);
+export default ProductModel; 4
